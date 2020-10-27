5defe4bee2e463164facc65ab6f4f7afab9825ef
Lu <1625196521@qq.com>
2020-10-27 19:51:30 +0800

    static page

--- src/App/App.js
+++ src/App/App.js
@@ -2,10 +2,73 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentList: [],
+      teamList: [],
+    };
+  }
+
+  // splitStudent = () => {
+  //   console.log('huafenxueyuan');
+  // };
+  // addStudent = () => {
+  //   console.log('add student');
+  // };
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <div className="container">
+          <div className="teamList">
+            <div className="title">
+              <button
+                type="button"
+                className="myButton"
+                // onClick={() => {
+                //   this.splitStudent;
+                // }}
+              >
+                分组学员
+              </button>
+              <h2>分组列表</h2>
+            </div>
+            <div>
+              {this.state.teamList.map((team, index) => {
+                return (
+                  <div key={index} className="team">
+                    <div className="teamTitle">{index + 1} 组</div>
+                    <div />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="studentList">
+            <div className="title">
+              <h2>学员列表</h2>
+            </div>
+            <div>
+              {this.state.studentList.map((student, index) => {
+                return (
+                  <span key={index} className="student">
+                    {student.id} {student.name}
+                  </span>
+                );
+              })}
+              <span>
+                <button
+                  type="button"
+                  // onClick={() => {
+                  //   this.addStudent;
+                  // }}
+                >
+                  +添加学员
+                </button>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
